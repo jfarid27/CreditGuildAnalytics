c9c8d594a536d5ecb80d18707d6dde6313cd29b0
jrab227@gmail.com <jrab227@gmail.com>
2024-09-01 23:58:36 -0400

    Adding surplus guild minter logic.

---
+++ models/CreditGuild/SurplusGuildMinter.py
@@ -0,0 +1,15 @@
+import json
+from ape import Contract
+from eth_utils import from_wei
+
+class SurplusGuildMinter:
+    """Interface to a Surplus Guild Minter for a specific credit market."""
+    def __init__(self, contract_address:str,
+                 abi_file:str="contracts/CreditGuild/SurplusGuildMinterABI.json"):
+        self.contract = Contract(contract_address, abi=abi_file)
+    
+    def fetch_user_stake(self, user_address:str, term_address:str):
+        """Return the total stake for the user in the given term for this SGM."""
+        params= [user_address, term_address]
+        stake_details = self.contract.getUserStake(**params)
+        return from_wei(stake_details['credit'], 'ether')
